f18bc6dd69db281a3e7df3943b1eb4b91e120818
sphurtishikre4 <sphurtishikre4@gmail.com>
2025-01-25 13:33:45 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,7 @@
+const bttn= document.getElementById('resbtn');
+const img = document.getElementById('resimg');
+  button = addEventListener('click',()=>{
+      img.style.display="block";
+
+  });
+  
